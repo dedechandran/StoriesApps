f4897fd61bae648c51f060df70a04a402eaaa22b
dedechandran <dedechandra852.dc@gmail.com>
2022-06-12 20:54:54 +0700

    Add home screen

---
+++ app/src/main/java/com/dedechandran/storiesapps/presentation/home/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.dedechandran.storiesapps.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dedechandran.storiesapps.R
+import com.dedechandran.storiesapps.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.bind(view)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(viewLifecycleOwner) {
+
+        }
+    }
+}
